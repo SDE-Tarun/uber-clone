5760949a9d99d3080fa954cb4f58d12d2e90aae6
Tarun <tarunkumar23j@gmail.com>
2024-12-09 15:12:57 +0530

    User model, new user registration

---
+++ backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minlength: [3, 'First name must be atleast 3 characters long']
+        },
+        lastname: {
+            type: String,
+            minlength: [3, 'Last name must be atleast 3 characters long']
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: [5, 'Email must be atleast 5 characters long']
+    },
+    password: {
+        type: String,
+        required: true,
+        // Whenever we find a user, we dont want to share password so for it we use select:false 
+        select: false
+    },
+    // Used for live tracking, so that driver or captain live location is shared or tracked by user
+    socketId: {
+        type: String
+    }
+})
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY) 
+    return token;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
+
