a112fcca1a43ad391dc6a43ee2ace8053e591d62
Tarun <tarunkumar23j@gmail.com>
2024-12-12 21:47:51 +0530

    App.jsx, main.jsx and UserSignup.jsx file has been formatted

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -107,7 +107,8 @@ const UserSignup = () => {
       </div>
       <div>
         <p className="text-[10px] leading-tight">
-          This site is protected by reCAPTCHA and the <span className="underline">Google Privacy Policy</span> and
+          This site is protected by reCAPTCHA and the{" "}
+          <span className="underline">Google Privacy Policy</span> and
           <span className="underline">Terms of Service apply</span>.
         </p>
       </div>
